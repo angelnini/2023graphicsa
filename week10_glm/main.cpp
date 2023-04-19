8d46673e27e87e18d3d4158f282decf11f0c0472
angelnini <91250186+angelnini@users.noreply.github.com>
2023-04-19 12:04:00 +0800

    week10
    week10

---
+++ week10_glm/main.cpp
@@ -0,0 +1,24 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel*pmodel=NULL;///有一個模型的指標，先是空的
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(pmodel==NULL){
+        pmodel = glmReadOBJ("al.obj");
+        glmUnitize(pmodel);
+        glmFacetNormals(pmodel);
+        glmVertexNormals(pmodel, 90.0);
+    }
+    glmDraw(pmodel,GLM_MATERIAL);///從
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week10");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
