d677592129fcb5438cf5ada669c3603c405a4c53
angelnini <angetso1229@gmail.com>
2023-05-17 12:23:39 +0800

    week14

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <GL/glut.h>
-
 #include "glm.h"
 GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
-GLMmodel * lowarmR = NULL;///week13-02-1
-int show[4]={0,1,0,0};
+GLMmodel * lowarmR = NULL;
+int show[4]={1,1,1,1};///用show[i]決定要不要顯示
+int ID=3;///0:頭 1:身體 2:上手臂 3:下手臂
 void keyboard (unsigned char key, int x, int y){
-    if (key=='0') show[0]=!show[0];
-    if (key=='1') show[1]=!show[1];
-    if (key=='2') show[2]=!show[2];
-    if (key=='3') show[3]=!show[3];
+    if (key=='0') ID = 0;
+    if (key=='1') ID = 1;
+    if (key=='2') ID = 2;
+    if (key=='3') ID = 3;
+
     glutPostRedisplay();
 }
 FILE * fout = NULL; ///一開始,檔案沒有開, NULL
@@ -25,40 +26,65 @@ void display()
         glScalef(0.2, 0.2, 0.2);///week13-02-2
         if(body==NULL){
             head=glmReadOBJ("model/head.obj");
-            body=glmReadOBJ("model/body.obj");///week13-02-1
-            uparmR=glmReadOBJ("model/uparmR.obj");///week13-03-1
-            lowarmR=glmReadOBJ("model/lowarmR.obj");///week13-03-1
+            body=glmReadOBJ("model/body.obj");
+            uparmR=glmReadOBJ("model/uparmR.obj");
+            lowarmR=glmReadOBJ("model/lowarmR.obj");
             ///glmUnitize(body);///week13-02-1這行之後會改
         }
-        if (show[0]) glmDraw(head, GLM_MATERIAL);///week13-03-1
+        if(ID==0) glColor3f(1,0,0);///選定的，設紅色
+        else glColor3f(1,1,1);
+        if (show[0]) glmDraw(head, GLM_MATERIAL);
+
+        if(ID==1) glColor3f(1,0,0);///選定的，設紅色
+        else glColor3f(1,1,1);
+        if (show[1]) glmDraw(body, GLM_MATERIAL);
+
         glPushMatrix();
-            //
-            if (show[1]) glmDraw(body, GLM_MATERIAL);
+            glTranslatef(-1.200000, +0.453333, 0);
+            glRotatef(angle, 0, 0, 1);
+            ///glTranslatef(teapotX, teapotY, 0);
+            glTranslatef(1.200000, -0.453333, 0);
+
+            if(ID==2) glColor3f(1,0,0);///選定的，設紅色
+            else glColor3f(1,1,1);
+            if (show[2]) glmDraw(uparmR, GLM_MATERIAL);
+            glPushMatrix();
+            glTranslatef(-1.946666, +0.180001, 0);
+            //glTranslatef(teapotX, teapotY, 0);
+            glRotatef(angle, 0, 0, 1);
+            glTranslatef(1.946666, -0.180001, 0);
+            if(ID==3) glColor3f(1,0,0);///選定的，設紅色
+            else glColor3f(1,1,1);
+            if (show[3]) glmDraw(lowarmR, GLM_MATERIAL);
         glPopMatrix();
-        if (show[2]) glmDraw(uparmR, GLM_MATERIAL);///week13-03-1
-        if (show[3]) glmDraw(lowarmR, GLM_MATERIAL);///week13-03-1
+        glPopMatrix();
+
+
     glPopMatrix();
+    glColor3f(0,1,0);///放小茶壺在中心點當參考
+    glutSolidTeapot(0.02);///放小茶壺在中心點當參考
     glutSwapBuffers();
 }
 int oldX=0, oldY=0;///week13-03-2
-void motion(int x, int y){///week13-03-2
-    teapotX += (x -oldX)/150.0;///week13-03-2
-    teapotY -= (y -oldY)/150.0;///week13-03-2
+void motion(int x, int y){
+    teapotX += (x -oldX)/150.0;
+    teapotY -= (y -oldY)/150.0;
     oldX=x;
     oldY=y;
+    angle = x;
     printf("glTranslatef(%f, %f, 0);\n",teapotX,teapotY);
-    glutPostRedisplay();///week13-03-2
+    glutPostRedisplay();
+
 }
 
 void mouse(int button, int state, int x, int y)
 {
     if(state==GLUT_DOWN){
-        oldX = x;///teapotX = (x-150)/150.0;
-        oldY = y;///teapotY = (150-y)/150.0;
+        teapotX = (x-150)/150.0;///teapotX = (x-150)/150.0;
+        teapotY = (150-y)/150.0;///teapotY = (150-y)/150.0;
         angle = x;
-        ///printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
-        ///if(fout==NULL) fout = fopen("file4.txt", "w"); ///step02-2 沒開檔,就開
-        ///fprintf(fout, "%f %f\n", teapotX, teapotY); ///step02-2 要再存座標
+        if(fout==NULL) fout = fopen("file4.txt", "w"); ///step02-2 沒開檔,就開
+        fprintf(fout, "%f %f\n", teapotX, teapotY); ///step02-2 要再存座標
     }
     display();
 }
@@ -75,7 +101,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week13");
+    glutCreateWindow("week14");
 
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
